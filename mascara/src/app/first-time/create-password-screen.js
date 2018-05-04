42ede59da160d95870488fece68e8ccf89cca6be
sourcecodesmith <herman@sourcecodesmith.com>
2018-05-04 17:10:47 +0200

    semi colons etc

--- mascara/src/app/first-time/create-password-screen.js
+++ mascara/src/app/first-time/create-password-screen.js
@@ -1,18 +1,18 @@
-import React, { Component } from 'react'
-import PropTypes from 'prop-types'
-import {connect} from 'react-redux'
-import { withRouter } from 'react-router-dom'
-import { compose } from 'recompose'
-import { createNewVaultAndKeychain } from '../../../../ui/app/actions'
-import Breadcrumbs from './breadcrumbs'
-import EventEmitter from 'events'
-import Mascot from '../../../../ui/app/components/mascot'
-import classnames from 'classnames'
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import {connect} from 'react-redux';
+import { withRouter } from 'react-router-dom';
+import { compose } from 'recompose';
+import { createNewVaultAndKeychain } from '../../../../ui/app/actions';
+import Breadcrumbs from './breadcrumbs';
+import EventEmitter from 'events';
+import Mascot from '../../../../ui/app/components/mascot';
+import classnames from 'classnames';
 import {
   INITIALIZE_UNIQUE_IMAGE_ROUTE,
   INITIALIZE_IMPORT_WITH_SEED_PHRASE_ROUTE,
   INITIALIZE_NOTICE_ROUTE,
-} from '../../../../ui/app/routes'
+} from '../../../../ui/app/routes';
 
 class CreatePasswordScreen extends Component {
   static propTypes = {
@@ -21,7 +21,7 @@ class CreatePasswordScreen extends Component {
     history: PropTypes.object.isRequired,
     isInitialized: PropTypes.bool,
     isUnlocked: PropTypes.bool,
-    isMascara: PropTypes.bool.isRequired,
+    isMascara: PropTypes.bool.isRequired
   }
 
   state = {
@@ -30,44 +30,44 @@ class CreatePasswordScreen extends Component {
   }
 
   constructor (props) {
-    super(props)
-    this.animationEventEmitter = new EventEmitter()
+    super(props);
+    this.animationEventEmitter = new EventEmitter();
   }
 
   componentWillMount () {
-    const { isInitialized, history } = this.props
+    const { isInitialized, history } = this.props;
 
     if (isInitialized) {
-      history.push(INITIALIZE_NOTICE_ROUTE)
+      history.push(INITIALIZE_NOTICE_ROUTE);
     }
   }
 
   isValid () {
-    const { password, confirmPassword } = this.state
+    const { password, confirmPassword } = this.state;
 
     if (!password || !confirmPassword) {
-      return false
+      return false;
     }
 
     if (password.length < 8) {
-      return false
+      return false;
     }
 
-    return password === confirmPassword
+    return password === confirmPassword;
   }
 
   createAccount = () => {
     if (!this.isValid()) {
-      return
+      return;
     }
 
-    const { password } = this.state
-    const { createAccount, history } = this.props
+    const { password } = this.state;
+    const { createAccount, history } = this.props;
 
     this.setState({ isLoading: true })
     createAccount(password)
-      .then(() => history.push(INITIALIZE_UNIQUE_IMAGE_ROUTE))
-  }
+      .then(() => history.push(INITIALIZE_UNIQUE_IMAGE_ROUTE));
+  };
 
   renderFields () {
     const { isMascara, history } = this.props
